481e0ddc8c7c85a6c994ea1de6a00a5e09dc8224
Adam Szeptycki <adamszeptycki@gmail.com>
2015-03-11 13:48:32 +0100

    adds support for abstract classes when assigning values from nsdictionary

--- PCDatabaseCore/Categories/NSManagedObject+ImportFromJSON.m
+++ PCDatabaseCore/Categories/NSManagedObject+ImportFromJSON.m
@@ -10,25 +10,31 @@
 #import <objc/runtime.h>
 
 @implementation NSManagedObject (ImportFromJSON)
+
 - (void)setValuesForKeysWithJSONDictionary:(NSDictionary *)keyedValues
 {
     unsigned int propertyCount;
-    objc_property_t *properties = class_copyPropertyList([self class], &propertyCount);
+    Class class = [self class];
     
-    for (int i=0; i<propertyCount; i++) {
-        objc_property_t property = properties[i];
-        const char *propertyName = property_getName(property);
-        NSString *keyName = [NSString stringWithUTF8String:propertyName];
-        
-        if ([keyName isEqualToString:@"id"])
-            keyName = @"dbId";
-        
-        id value = [keyedValues objectForKey:keyName];
-        if (value != nil) {
-            [self setValue:value forKey:keyName];
+    while (class != NSManagedObject.class) {
+        objc_property_t *properties = class_copyPropertyList(class, &propertyCount);
+        for (int i=0; i<propertyCount; i++) {
+            objc_property_t property = properties[i];
+            const char *propertyName = property_getName(property);
+            NSString *keyName = [NSString stringWithUTF8String:propertyName];
+            
+            if ([keyName isEqualToString:@"id"])
+                keyName = @"dbId";
+            
+            id value = [keyedValues objectForKey:keyName];
+            if (value != nil) {
+                [self setValue:value forKey:keyName];
+            }
         }
+        free(properties);
+        class = [class superclass];
     }
-    free(properties);
 }
+
 
 @end
